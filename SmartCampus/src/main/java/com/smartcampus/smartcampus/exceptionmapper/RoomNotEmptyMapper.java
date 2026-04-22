908cf549af42787f19e55d18322e8bef6ec70cae
TeneeIshari <teneesha.alwis@gmail.com>
2026-04-22 11:23:56 +0530

    Created RoomNotEmptydMapper file

---
+++ SmartCampus/src/main/java/com/smartcampus/smartcampus/exceptionmapper/RoomNotEmptyMapper.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.smartcampus.exceptionmapper;
+
+import com.smartcampus.smartcampus.exception.SmartCampusException.RoomNotEmptyException;
+import com.smartcampus.smartcampus.model.ErrorMessage;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Maps RoomNotEmptyException to a 409 Conflict response.
+ * Handles Part 5.1 of the coursework.
+ */
+@Provider
+public class RoomNotEmptyMapper implements ExceptionMapper<RoomNotEmptyException> {
+
+    @Override
+    public Response toResponse(RoomNotEmptyException exception) {
+        ErrorMessage errorMessage = new ErrorMessage(
+                exception.getMessage(),
+                409,
+                "https://smartcampus.edu/api/docs/errors/409"
+        );
+
+        return Response.status(Status.CONFLICT)
+                .entity(errorMessage)
+                .build();
+    }
+}
